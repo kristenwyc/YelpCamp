beb3b6722066928635c119a10a3d9713ab221de2
Kristen <105521957+kristenwcheung@users.noreply.github.com>
2022-08-22 17:27:09 -0400

    added delete button for reviews and a delete middleware

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 
@@ -7,7 +8,23 @@ const CampgroundSchema = new Schema ({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'  //from the review model
+        }
+    ]
+})
+
+CampgroundSchema.post('findOneAndDelete', async function(doc) {   //Mongoose Middleware, "doc" refers to the deleted campground info; this is a query middleware
+    if(doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 })
 
 module.exports = mongoose.model('Campground', CampgroundSchema)
