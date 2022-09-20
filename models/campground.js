46a042f0a31e9c5352e84cb2da7ddb7aaac49fdb
Kristen <105521957+kristenwcheung@users.noreply.github.com>
2022-09-19 22:59:16 -0400

    added mapbox, stylesheet for reviews, updated campground model, etc

--- models/campground.js
+++ models/campground.js
@@ -1,19 +1,32 @@
 const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
- 
+
 const ImageSchema = new Schema({
-        url: String,
-        filename: String
+    url: String,
+    filename: String
 });
 
-ImageSchema.virtual('thumbnail').get(function() {
+ImageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('/upload', '/upload/w_200');  //"replace" only replaces the first match
 })
 
-const CampgroundSchema = new Schema ({
+const opts = { toJSON: { virtuals: true } };
+
+const CampgroundSchema = new Schema({
     title: String,
     images: [ImageSchema],
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
     price: Number,
     description: String,
     location: String,
@@ -26,11 +39,19 @@ const CampgroundSchema = new Schema ({
             type: Schema.Types.ObjectId,
             ref: 'Review'  //from the review model
         }
-    ]
-})
+    ],
+}, opts);
 
-CampgroundSchema.post('findOneAndDelete', async function(doc) {   //Mongoose Middleware, "doc" refers to the deleted campground info; this is a query middleware
-    if(doc) {
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+    <p>${this.description.substring(0, 20)}...</p>`
+});
+
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {   //Mongoose Middleware, "doc" refers to the deleted campground info; this is a query middleware
+    if (doc) {
         await Review.deleteMany({
             _id: {
                 $in: doc.reviews
--- seeds/index.js
+++ seeds/index.js
@@ -6,23 +6,44 @@ const Campground = require('../models/campground');
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/yelp-camp');
-    console.log('Database connected!!')
-  }
+  await mongoose.connect('mongodb://localhost:27017/yelp-camp');
+  console.log('Database connected!!')
+}
 
 const sample = array => array[Math.floor(Math.random() * array.length)]
 
 
 const seedDB = async () => {
-    await Campground.deleteMany({})
-    for (let i = 0; i <50; i++) {
-      const random1000 = Math.floor(Math.random() *1000)
-      const camp = new Campground({
-        location: `${cities[random1000].city}, ${cities[random1000].state}`,
-        title: `${sample(descriptors)} ${sample(places)}`
-      })
-      await camp.save();
-    }
+  await Campground.deleteMany({})
+  for (let i = 0; i < 300; i++) {
+    const random1000 = Math.floor(Math.random() * 1000)
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      author: '6317cd07e91282b8bd343d69',
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut expedita similique quo ut consequuntur. Perspiciatis quasi dolorum nesciunt numquam unde rem dolore praesentium odio. Maiores rerum minus laboriosam impedit dolore!',
+      price,
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude
+        ]
+      },
+      images: [
+        {
+          url: 'https://res.cloudinary.com/dfweogqga/image/upload/v1663278641/YelpCamp/ojdzkmsgqllr4xehlavz.jpg',
+          filename: 'YelpCamp/ojdzkmsgqllr4xehlavz'
+        },
+        {
+          url: 'https://res.cloudinary.com/dfweogqga/image/upload/v1663278641/YelpCamp/sqfncdhnqmgj9tcni10q.png',
+          filename: 'YelpCamp/sqfncdhnqmgj9tcni10q'
+        }
+      ]
+    })
+    await camp.save();
+  }
 }
 
 seedDB().then(() => {
